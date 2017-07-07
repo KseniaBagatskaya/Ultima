4cc496f8acbdbfebee0fc35b03a0f3850d803654
OktavianRS <oktavianrs@gmail.com>
2017-07-07 18:16:55 +0300

    created tabs templates created abstract routes

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -12,6 +12,14 @@
         $urlRouterProvider.otherwise('/login');
 
         $stateProvider
+            .state('app', {
+                abstract: true,
+                templateUrl: 'templates/header/header.html',
+            })
+            .state('app.tabs', {
+                abstract: true,
+                templateUrl: 'templates/tabs/tabs.html',
+            })
             .state('login', {
                 url: '/login',
                 templateUrl: 'templates/login/login.html',
@@ -34,6 +42,74 @@
 
                     }
                 }
+            })
+            .state('app.tabs.antragsteller', {
+                url: "/antragsteller",
+                templateUrl: 'templates/antragsteller/antragsteller.html',
+                controller: 'AntragstellerController',
+                controllerAs: 'vm',
+                // resolve: {
+
+                //     antragsteller_data: function (dashboard) {
+                //         return dashboard.getAllMembers()
+                //             .then(function (res) {
+                //                 console.log(res, 'res');
+                //                 return res;
+                //             });
+
+                //     }
+                // }
+            })
+            .state('immobilie', {
+                url: "/immobilie",
+                templateUrl: 'templates/immobilie/immobilie.html',
+                controller: 'ImmobilieController',
+                controllerAs: 'vm',
+                // resolve: {
+
+                //     antragsteller_data: function (dashboard) {
+                //         return dashboard.getAllMembers()
+                //             .then(function (res) {
+                //                 console.log(res, 'res');
+                //                 return res;
+                //             });
+
+                //     }
+                // }
+            })
+            .state('kreditdaten', {
+                url: "/kreditdaten",
+                templateUrl: 'templates/kreditdaten/kreditdaten.html',
+                controller: 'KreditdatenController',
+                controllerAs: 'vm',
+                // resolve: {
+
+                //     antragsteller_data: function (dashboard) {
+                //         return dashboard.getAllMembers()
+                //             .then(function (res) {
+                //                 console.log(res, 'res');
+                //                 return res;
+                //             });
+
+                //     }
+                // }
+            })
+            .state('dokumente', {
+                url: "/dokumente",
+                templateUrl: 'templates/dokumente/dokumente.html',
+                controller: 'DokumenteController',
+                controllerAs: 'vm',
+                // resolve: {
+
+                //     antragsteller_data: function (dashboard) {
+                //         return dashboard.getAllMembers()
+                //             .then(function (res) {
+                //                 console.log(res, 'res');
+                //                 return res;
+                //             });
+
+                //     }
+                // }
             })
 
 
---
+++ app/templates/antragsteller/antragsteller.controller.js
@@ -0,0 +1,16 @@
+;(function () {
+
+    'use strict';
+
+    angular.module('app')
+        .controller('AntragstellerController', AntragstellerController);
+
+    AntragstellerController.$inject = ['$scope'];
+
+
+    function AntragstellerController($scope) {
+        var vm = this;
+
+    }
+
+})();
