11ba744ec324bff0a94608602548e71b1dd7981a
Vista <vista-54@ukr.net>
2017-07-07 20:03:25 +0300

    Added kinder directive

--- app/templates/antragsteller/antragsteller.controller.js
+++ app/templates/antragsteller/antragsteller.controller.js
@@ -11,17 +11,33 @@
     function AntragstellerController($scope) {
         let vm = this;
         vm.submit = submit;
+        vm.addKinder = addKinder;
+        vm.deleteKinder = deleteKinder;
+
+
         vm.antragsteller1 = {
             number: '1'
         };
         vm.antragsteller2 = {
             number: '2'
         };
+        vm.kinders = [];
 
 
         function submit() {
             console.log(vm.antragsteller1);
             console.log(vm.antragsteller2);
+            console.log(vm.kinders);
+        }
+
+        function addKinder() {
+            vm.kinders.push({
+                _delete: deleteKinder
+            });
+        }
+
+        function deleteKinder(index) {
+            vm.kinders.splice(index, 1);
         }
     }
 
