fb56ba2e09e8f76bb1453c20f65662f5aa263702
OktavianRS <oktavianrs@gmail.com>
2017-07-10 03:39:40 +0300

    12

--- app/templates/tabs/tabs.controller.js
+++ app/templates/tabs/tabs.controller.js
@@ -5,14 +5,14 @@
     angular.module('app')
         .controller('TabsController', TabsController);
 
-    TabsController.$inject = ['$scope', '$state', '$rootScope'];
+    TabsController.$inject = ['$scope', '$state', '$rootScope', '$stateParams'];
 
 
-    function TabsController($scope, $state, $rootScope) {
+    function TabsController($scope, $state, $rootScope, $stateParams) {
         var vm = this;
 
         vm.transactionId = sessionStorage.getItem('transactionId');
-        vm.params_id = $state.params.id;
+        vm.params_id = $stateParams.id;
         vm.current_controller = $state.current.controller;
         
         // if state changed relect active tab
