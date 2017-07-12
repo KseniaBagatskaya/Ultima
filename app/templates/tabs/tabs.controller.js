50be08317b3ce6e4832166204e0155d94f6066d3
Vista <vista-54@ukr.net>
2017-07-12 15:37:42 +0300

    tab fixed

--- app/templates/tabs/tabs.controller.js
+++ app/templates/tabs/tabs.controller.js
@@ -12,16 +12,16 @@
         var vm = this;
 
         vm.transactionId = sessionStorage.getItem('transactionId');
-        vm.params_id = $stateParams.id;
+        vm.params_id = $state.params.id;
         vm.current_controller = $state.current.controller;
 
         // if state changed relect active tab
         $rootScope.$on('$stateChangeStart',
-        (event, toState, toParams, fromState, fromParams) => {
-            vm.current_controller = toState.controller;
-            vm.params_id = toParams.id;
-            // vm.params_id = fromParams.id;
-        })
+            (event, toState, toParams, fromState, fromParams) => {
+                vm.current_controller = toState.controller;
+                vm.params_id = toParams.id === '' ? fromParams.id : toParams.id;
+                // vm.params_id = fromParams.id;
+            })
 
     }
 
