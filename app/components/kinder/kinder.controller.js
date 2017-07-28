9997777619ccc73b0e6a77383a9df78e01c9f56d
Edward Arinin <oktavianrs@gmail.com>
2017-07-28 13:41:51 +0300

    Fixed reverted changes. Fixed kinders list

--- app/components/kinder/kinder.controller.js
+++ app/components/kinder/kinder.controller.js
@@ -11,8 +11,13 @@
         let vm = this;
         vm.data = $scope.parent;
         vm.index = $scope.index;
+        vm.array = $scope.array;
+        vm.deleteKinder = deleteKinder;
         vm.convertDateFromString = antragsteller.convertDateFromString;
 
+        function deleteKinder(index) {
+            vm.array.splice(index, 1);
+        }
     }
 
 })();
